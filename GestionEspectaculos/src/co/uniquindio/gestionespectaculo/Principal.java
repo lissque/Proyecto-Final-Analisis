d1673966cf672e28c630e609f1de69e07e2eea46
Lissette Quebrada <lissque604@gmail.com>
2019-11-07 16:44:07 -0500

    Comentario hola
    hola en principal

--- GestionEspectaculos/src/co/uniquindio/gestionespectaculo/Principal.java
+++ GestionEspectaculos/src/co/uniquindio/gestionespectaculo/Principal.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 public class Principal extends Application {
 	private Stage escenarioPrincipal;
 	private Evento miEvento;
+	//HOLA
 
 	public static void main(String[] args) {
 		launch(args);
